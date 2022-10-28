5140f2a868a087e6421db91758be0abd76b3ddb9
Osvaldito <osvaldo.devmate@gmail.com>
2022-10-28 09:17:29 -0300

    first commit

---
+++ 02-JsTeorico/deepJsSpec.js
@@ -0,0 +1,16 @@
+describe('JavaScript teórico y su comportamiento', function() {
+
+// En este ejercicio vamos a trabajar distinto, las funciones en deepJs.js ya están armadas,
+// lo que que hay que hacer es poner el resultado correcto del test.
+// Traten de primero pensarlo y armarse una respuesta que puedan justificar.
+// Van a tener que considerar scopes, hoisting y todo lo que aprendieron de la teoria.
+
+  describe('Scopes de función y bloque', function() {
+   
+  });
+
+  describe('Precedencia y Asociatividad', function() {
+
+   
+  })
+});
